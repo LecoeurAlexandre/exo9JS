438324dda22495f453d1c95a52ec8dab50666be9
Alexandre Lecoeur <alexandre.lecoeur@gmail.com>
2023-01-11 09:31:56 +0100

    done

---
+++ main.js
@@ -0,0 +1,23 @@
+let age = Number(prompt("Veuillez saisir votre âge :"));
+let seniority = Number(prompt("Veuillez saisir votre ancienneté :"));
+let lastSalary = Number(prompt("Veuillez saisir votre dernier salaire :"));
+let indemnity;
+
+if (seniority >= 1 && seniority <=10 ) {
+    indemnity = lastSalary/2 * seniority;
+    if(age >= 46 && age <= 49) {
+        indemnity = indemnity + 2*lastSalary
+    } else if (age >= 50){
+        indemnity = indemnity + 5*lastSalary
+    }
+    alert(`L'indemnité de licenciement est de : ${indemnity}€`)
+
+} else {
+    indemnity = lastSalary * seniority;
+    if(age >= 46 && age <= 49) {
+        indemnity = indemnity + 2*lastSalary
+    } else if (age >= 50){
+        indemnity = indemnity + 5*lastSalary
+    }
+    alert(`L'indemnité de licenciement est de : ${indemnity}€`)
+}
